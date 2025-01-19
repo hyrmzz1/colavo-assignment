9fcb7a695784d25eb1c8457ad9e1eb9c0cb78bbe
Hyerim Yang <hlhlstar@naver.com>
2025-01-19 18:22:32 +0900

    feat: CartPage에 시술 및 할인 버튼 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,25 +5,18 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'px-4 py-3 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        primary: 'bg-purple text-white',
+        primary: 'bg-purple text-white py-2',
         secondary: 'bg-purple-light text-white',
-        gray: 'bg-gray-light text-gray',
-        red: 'bg-red-light text-red',
-      },
-      size: {
-        default: 'h-9 px-4 py-2',
-        sm: 'h-8 rounded-md px-3 text-xs',
-        lg: 'h-10 rounded-md px-8',
-        icon: 'h-9 w-9',
+        gray: 'bg-gray-light text-gray rounded-lg text-xs',
+        red: 'bg-red-light text-red rounded-lg text-xs',
       },
     },
     defaultVariants: {
       variant: 'primary',
-      size: 'default',
     },
   }
 );
@@ -35,11 +28,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, className }))}
         ref={ref}
         {...props}
       />
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,32 @@
+import useViewStore from '@/stores/useViewStore';
+import { Button } from '@/components/ui/button';
+import { IoIosAddCircle } from 'react-icons/io';
+
 const CartPage = () => {
-  return <></>;
+  const setView = useViewStore((state) => state.setView);
+
+  const buttons = [
+    { variant: 'gray', view: 'service', label: '시술' },
+    { variant: 'red', view: 'discount', label: '할인' },
+  ] as const;
+
+  return (
+    <>
+      <div className='flex gap-2 px-4'>
+        {buttons.map(({ variant, view, label }) => (
+          <Button
+            className='w-full gap-1'
+            variant={variant}
+            onClick={() => setView(view)}
+          >
+            <IoIosAddCircle />
+            {label}
+          </Button>
+        ))}
+      </div>
+      <main>{/* 장바구니 목록 렌더링 */}</main>
+    </>
+  );
 };
 
 export default CartPage;
