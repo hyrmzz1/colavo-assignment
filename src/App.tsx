6e01928d72c5b3a4cf20c1f95f75e3a8349c4438
Hyerim Yang <hlhlstar@naver.com>
2025-01-19 19:03:44 +0900

    chore: react-query, react-query-devtools 설치 및 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from './layouts/Layout';
 import Page from './pages/Page';
 
-const App = () => {
-  return (
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
     <Layout>
       <Page />
     </Layout>
-  );
-};
+    {import.meta.env.MODE === 'development' && (
+      <ReactQueryDevtools initialIsOpen={false} />
+    )}
+  </QueryClientProvider>
+);
 
 export default App;
